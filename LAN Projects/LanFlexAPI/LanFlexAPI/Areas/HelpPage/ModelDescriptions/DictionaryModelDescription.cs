dc1cf938cc8f38aa69318f43cf7c8ce1746f0ea0
Shubham Sharma <ssharma0834@gmail.com>
2022-09-14 16:52:14 +0530

    removed old projects

--- LAN Projects/LanFlexAPI/LanFlexAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace LanFlexAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- LAN Projects/LanFlexAPI/LanFlexAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace LanFlexAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
